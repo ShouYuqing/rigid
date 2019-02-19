40855b45b742adc271e376ec2c87e8283bfcc40b
yq_mc <573380218@qq.com>
2019-02-19 16:29:20 -0500

    Update rigid_train.py

--- src/rigid_train.py
+++ src/rigid_train.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : ${DATE} ${TIME}
+# @Author  : Aries
+# @Site    : ${SITE}
+# @File    : ${NAME}.py
+# @Software: ${PRODUCT_NAME}
+
 # python imports
 import os
 import glob
