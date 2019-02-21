9ed4b267c5d5edda9d8da252624717e5524a5285
yq_mc <573380218@qq.com>
2019-02-20 23:41:35 -0500

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -119,8 +119,14 @@ def rigid_net(vol_size, enc_nf, dec_nf):
     [src, tgt] = unet_model.inputs
     x_out = unet_model.outputs[-1]
     # affine transform matrix
-    # how to output a flow with the affine transform matrix????
-    flow =
+    # build full connected layer into the model, output the affine matrix
+    flow = Conv3D(3, kernel_size=3, padding='same',
+                  kernel_initializer=RandomNormal(mean=0.0, stddev=1e-5), name='flow')(x_out)
+    
+    flow1 = flow[0,:,:,:,0]#(160,192,224)
+    flow2 = flow[0,:,:,:,1]
+    flow3 = flow[0,:,:,:,2]
+
     # spatial transform
     y = nrn_layers.SpatialTransformer(interp_method='linear', indexing='xy')([src, flow])
 
