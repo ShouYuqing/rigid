b00072e2b2cd1c349ca4937f7f8e2d7a31b89eea
yq_mc <573380218@qq.com>
2019-02-21 00:16:32 -0500

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -122,11 +122,11 @@ def rigid_net(vol_size, enc_nf, dec_nf):
     # build full connected layer into the model, output the affine matrix
     flow = Conv3D(3, kernel_size=3, padding='same',
                   kernel_initializer=RandomNormal(mean=0.0, stddev=1e-5), name='flow')(x_out)
-    
     flow1 = flow[0,:,:,:,0]#(160,192,224)
     flow2 = flow[0,:,:,:,1]
     flow3 = flow[0,:,:,:,2]
-
+    # add convolutinal layer into the model
+    
     # spatial transform
     y = nrn_layers.SpatialTransformer(interp_method='linear', indexing='xy')([src, flow])
 
