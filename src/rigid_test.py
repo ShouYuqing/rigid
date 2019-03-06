37f1fe1b44fd65b2f7535a0377a1eaa9f0b8ade0
yq_mc <573380218@qq.com>
2019-03-06 17:06:57 -0500

    Update rigid_test.py

--- src/rigid_test.py
+++ src/rigid_test.py
@@ -56,7 +56,7 @@ def test( iter_num, gpu_id, vol_size=(160,192,224), nf_enc=[16,32,32,32], nf_dec
     x = np.reshape(np.linspace(0, vol_size[0], sample_num + 1), (1, sample_num + 1))
     y = np.reshape(np.linspace(0, vol_size[1], sample_num + 1), (1, sample_num + 1))
     z = np.reshape(np.linspace(0, vol_size[2], sample_num + 1), (1, sample_num + 1))#index
-    print(z.shape)
+    print(z)
     #index = np.array((np.meshgrid(x, y, z)))
 
     # Y in formula
@@ -65,9 +65,9 @@ def test( iter_num, gpu_id, vol_size=(160,192,224), nf_enc=[16,32,32,32], nf_dec
     z_flow = np.arange(vol_size[2])
     print(z_flow.shape)
     grid = np.array((np.meshgrid(x_flow, y_flow, z_flow)))#original coordinate
-    grid_x = grid[0, x, y, z]
-    grid_y = grid[1, x, y, z]
-    grid_z = grid[2, x, y, z]#(10,10,10)
+    grid_x = grid[0, np.linspace(0, vol_size[0], sample_num + 1), np.linspace(0, vol_size[1], sample_num + 1), np.linspace(0, vol_size[2], sample_num + 1)]
+    grid_y = grid[1, np.linspace(0, vol_size[0], sample_num + 1), np.linspace(0, vol_size[1], sample_num + 1), np.linspace(0, vol_size[2], sample_num + 1)]
+    grid_z = grid[2, np.linspace(0, vol_size[0], sample_num + 1), np.linspace(0, vol_size[1], sample_num + 1), np.linspace(0, vol_size[2], sample_num + 1)]#(10,10,10)
 
     sample = flow + grid
     sample = sample[x, y, z]
