339a72bbff608661e85a64292a27d798e683fc0a
yq_mc <573380218@qq.com>
2019-03-06 19:00:02 -0500

    Update rigid_test.py

--- src/rigid_test.py
+++ src/rigid_test.py
@@ -69,9 +69,9 @@ def test( iter_num, gpu_id, vol_size=(160,192,224), nf_enc=[16,32,32,32], nf_dec
     y_flow = np.arange(vol_size[1])
     z_flow = np.arange(vol_size[2])
     grid = np.array((np.meshgrid(x_flow, y_flow, z_flow)))#original coordinate
-    print(grid[0, :, :, :])
-    print(grid[1, :, :, :])
-    print(grid[2, :, :, :])
+    print(grid[0, :, :, :].shape)
+    print(grid[1, :, :, :].shape)
+    print(grid[2, :, :, :].shape)
     grid_x = grid_sample(x, y, z, grid[0, :, :, :], sample_num)
     grid_y = grid_sample(x, y, z, grid[1, :, :, :], sample_num)
     grid_z = grid_sample(x, y, z, grid[2, :, :, :], sample_num)# (10,10,10)
