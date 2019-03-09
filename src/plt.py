ed149fd35ce443505593257f5720f85498ea2e11
yq_mc <573380218@qq.com>
2019-03-09 15:42:30 -0500

    mc

---
+++ src/plt.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import glob
+
+# third party
+import scipy.io as sio
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.path.append('../ext/neuron')
+from neuron import plot
+
+img = np.load("../data/test_seg.npz")
+volume = img['vol_data']
+print(volume.shape)
+plt.figure()
+plot(volume[100, :, :])
+#plt.plot(volume[100,:,:])
+plt.savefig("1.png")
