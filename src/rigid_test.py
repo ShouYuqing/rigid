59ed3c3a246499e06cdd36274ea2c13b5078aaca
yq_mc <573380218@qq.com>
2019-03-14 19:08:07 -0400

    Update rigid_test.py

--- src/rigid_test.py
+++ src/rigid_test.py
@@ -24,7 +24,7 @@ import neuron.layers as nrn_layers
 import neuron.utils as util
 import neuron.plot as nplt
 
-def test(iter_num, gpu_id, vol_size=(160,192,224), nf_enc=[16,32,32,32], nf_dec=[32,32,32,32,32,16,16,3], model_name = "vm2_cc", sample_num = 20, grid_dimension = 4):
+def test(iter_num, gpu_id, vol_size=(160,192,224), nf_enc=[16,32,32,32], nf_dec=[32,32,32,32,32,16,16,3], model_name = "vm2_cc", sample_num = 10, grid_dimension = 4):
     """
     Test of the rigid registration by calculating the dice score between the atlas's segmentation and warped image's segmentation
     :param iter_num: iteration number
@@ -125,7 +125,7 @@ def test(iter_num, gpu_id, vol_size=(160,192,224), nf_enc=[16,32,32,32], nf_dec=
     shifted_y = np.arange(vol_size[1])
     shifted_z = np.arange(vol_size[2])
     shifted_grid = np.rollaxis(np.array((np.meshgrid(shifted_y, shifted_x, shifted_z))), 0, 4)
-    
+
     for i in np.arange(vol_size[0]):
         for j in np.arange(vol_size[1]):
             for z in np.arange(vol_size[2]):
