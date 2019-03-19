77962306108bb4ec52cf8f260d9216fc3c0ce298
yq_mc <573380218@qq.com>
2019-03-19 15:35:47 -0400

    mc

--- src/rigid_test.py
+++ src/rigid_test.py
@@ -110,7 +110,7 @@ def test(iter_num, gpu_id, vol_size=(160,192,224), nf_enc=[16,32,32,32], nf_dec=
         for j in np.arange(sample_num):
             for z in np.arange(sample_num):
                 Y[i, j, z, :] = np.array([sample_x[i,j,z], sample_y[i,j,z], sample_z[i,j,z], 1])
-                Y[i, j, z, :] = Y[i, j, z, :] - T
+                Y[i, j, z, :] = Y[i, j, z, :] - T# amend: Y` = Y - T
 
     for i in np.arange(sample_num):
         for j in np.arange(sample_num):
