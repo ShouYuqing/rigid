a0183f9ebaa0f37f86336276528af57e46627e7e
yq_mc <573380218@qq.com>
2019-04-09 21:48:56 -0400

    mc

--- ext/image/image/aug_image.py
+++ ext/image/image/aug_image.py
@@ -3,6 +3,8 @@
 """
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interpn
 
 def rotate_img(img, vol_size, theta, beta ,omega):
     """
@@ -10,9 +12,9 @@ def rotate_img(img, vol_size, theta, beta ,omega):
     if do not want rotate: value equals 360
     :param img: original image
     :param vol_size: grid's size
-    :param theta: x
-    :param beta: y
-    :param omega: z
+    :param theta:
+    :param beta:
+    :param omega:
     :return: rotated image
     """
     # construct the transform matrix used for rotation
@@ -20,26 +22,25 @@ def rotate_img(img, vol_size, theta, beta ,omega):
     beta = (beta / 180) * math.pi
     omega = (omega / 180) * math.pi
     TX = np.array([[1, 0, 0, 0],
-                  [0, math.cos(beta), math.sin(beta), 0],
-                  [0, -math.sin(beta), math.cos(beta), 0],
+                  [0, math.cos(theta), math.sin(theta), 0],
+                  [0, -math.sin(theta), math.cos(theta), 0],
                   [0, 0, 0, 1]])
-    TY = np.array([[],
-                  [],
-                  [],
-                  []])
-    TZ = np.array([[],
-                  [],
-                  [],
-                  []])
-
-    T1 = np.array([[],
-                  [],
-                  [],
-                  []])
-    T2 = np.array([[],
-                  [],
-                  [],
-                  []])
+    TY = np.array([[math.cos(beta), 0, -math.sin(beta), 0],
+                  [0, 1, 0, 1],
+                  [math.sin(beta), 0, math.cos(beta), 0],
+                  [0, 0, 0, 1]])
+    TZ = np.array([[math.cos(omega), math.sin(omega), 0, 0],
+                  [-math.sin(omega), math.cos(omega), 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]])
+    T1 = np.array([[1, 0, 0, 0],
+                  [0 ,1, 0, 0],
+                  [0, 0, 1, 0],
+                  [-int(vol_size[0]/2), -int(vol_size[1]/2), -int(vol_size[2]/2), 1]])
+    T2 = np.array([[1, 0, 0, 0],
+                  [0 ,1, 0, 0],
+                  [0, 0, 1, 0],
+                  [int(vol_size[0]/2), int(vol_size[1]/2), int(vol_size[2]/2), 1]])
 
     # construct grid
     x = np.arange(vol_size[1])
@@ -49,14 +50,28 @@ def rotate_img(img, vol_size, theta, beta ,omega):
     for i in np.arange(vol_size[0]):
         for j in np.arange(vol_size[1]):
             for z in np.arange(vol_size[2]):
-                grid[i, j, z, 0] =
-                grid[i, j, z, 1] =
-                grid[i, j, z, 2] =
-
+                coordinates = np.dot(T2, np.dot(TZ, np.dot(TY, np.dot(TX, np.dot(T1, np.array([i, j, z, 1]).reshape(4, 1))))))
+                grid[i, j, z, 1] = coordinates[0]
+                grid[i, j, z, 0] = coordinates[1]
+                grid[i, j, z, 2] = coordinates[2]
 
     # interpolation
+    xx = np.arange(vol_size[1])
+    yy = np.arange(vol_size[0])
+    zz = np.arange(vol_size[2])
+    transformed_grid = np.stack((grid[:, :, :, 1], grid[:, :, :, 0], grid[:, :, :, 2]), 3)  # notice: the shifted_grid is reverse in x and y, so this step is used for making it back.
+    post_img = interpn((yy, xx, zz), img[:, :, :], transformed_grid, method='linear', bounds_error=False,
+                       fill_value=0)
+    return post_img
 
-
-    # construct return grid
-
-    return img
+if __name__ == "__main__":
+    img = np.load("atlas_norm.npz")
+    img = img["vol"]
+    print(img.shape)
+    rotated_img = rotate_img(img, vol_size=(160,192,224), theta = 20, beta = 20, omega = 20)
+    plt.figure()
+    plt.imshow(rotated_img[80, :, :])
+    plt.savefig("rotated.png")
+    plt.figure()
+    plt.imshow(img[80, :, :])
+    plt.savefig("origin.png")
