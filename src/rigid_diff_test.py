b99e26f1295d567ec79376ee90dc3b1beef555fd
yq_mc <573380218@qq.com>
2019-04-17 16:05:50 -0400

    mc

--- src/rigid_diff_test.py
+++ src/rigid_diff_test.py
@@ -158,7 +158,7 @@ def test(gpu_id, iter_num,
                 for j in np.arange(sample_num):
                     for z in np.arange(sample_num):
                         Y[i, j, z, :] = np.array([sample_x[i, j, z], sample_y[i, j, z], sample_z[i, j, z], 1])
-                        Y[i, j, z, :] = Y[i, j, z, :] - T  # amend: Y` = Y - T
+                        Y[i, j, z, :] = Y[i, j, z, :] - np.array([ave_x, ave_y, ave_z, 0])  # amend: Y` = Y - T
 
             for i in np.arange(sample_num):
                 for j in np.arange(sample_num):
